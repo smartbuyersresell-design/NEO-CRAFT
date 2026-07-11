5f4b170d4c14df74f8cab8b02330ae0366725503
smartbuyersresell-design <smartbuyers.resell@gmail.com>
2026-07-11 17:25:30 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,19 +1,7 @@
 (function () {
   'use strict';
   const W = window.NEO;
-  W.player = {
-    pos: new THREE.Vector3(0, 0, 0),
-    vel: new THREE.Vector3(0, 0, 0),
-    yaw: 0,
-    pitch: 0,
-    onGround: false,
-    height: 1.7,
-    radius: 0.32,
-    eye: 1.55,
-    health: 100,
-    hunger: 100,
-    alive: true
-  };
+  W.player = { pos: new THREE.Vector3(0,0,0), vel: new THREE.Vector3(0,0,0), yaw: 0, pitch: 0, onGround: false, height: 1.7, radius: 0.32, eye: 1.55, health: 100, hunger: 100, alive: true };
   W.G = { gravity: -22, jump: 8.2, speed: 5.2 };
   W.solidAt = (x, y, z) => {
     const b = W.getBlock(Math.floor(x), Math.floor(y), Math.floor(z));
@@ -21,8 +9,7 @@
   };
   W.collide = newPos => {
     const p = W.player, r = p.radius;
-    let x = p.pos.x, y = p.pos.y, z = p.pos.z;
-    let tx = newPos.x, tz = newPos.z, ty = newPos.y;
+    let x = p.pos.x, y = p.pos.y, z = p.pos.z, tx = newPos.x, tz = newPos.z, ty = newPos.y;
     p.onGround = false;
     if (!W.solidAt(tx + Math.sign(tx - x) * r, y, z) && !W.solidAt(tx + Math.sign(tx - x) * r, y + p.height - 0.2, z)) x = tx;
     if (!W.solidAt(x, y, tz + Math.sign(tz - z) * r) && !W.solidAt(x, y + p.height - 0.2, tz + Math.sign(tz - z) * r)) z = tz;
